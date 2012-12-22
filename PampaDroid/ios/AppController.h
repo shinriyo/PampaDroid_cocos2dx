d20d5c08fff1c425ccc29d9c17e6bfda35143fc3
shinriyo <shinriyo@gmail.com>
2012-12-22 20:28:42 +0900

    Initial Commit

---
+++ PampaDroid/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  PampaDroidAppController.h
+//  PampaDroid
+//
+//  Created by sugita on 12/12/22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ PampaDroid/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  PampaDroidAppController.h
+//  PampaDroid
+//
+//  Created by sugita on 12/12/22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
