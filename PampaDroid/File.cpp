d6ad047aa961ffca36968bfd30ada453f23fbd4d
shinriyo <shinriyo@gmail.com>
2013-02-10 20:16:04 +0900

    first commit

---
+++ PampaDroid/File.cpp
@@ -0,0 +1,9 @@
+//
+//  File.cpp
+//  PampaDroid
+//
+//  Created by sugita on 13/02/10.
+//
+//
+
+#include "File.h"
---
+++ PampaDroid/File.h
@@ -0,0 +1,14 @@
+//
+//  File.h
+//  PampaDroid
+//
+//  Created by sugita on 13/02/10.
+//
+//
+
+#ifndef __PampaDroid__File__
+#define __PampaDroid__File__
+
+#include <iostream>
+
+#endif /* defined(__PampaDroid__File__) */
